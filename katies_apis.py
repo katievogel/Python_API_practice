f133aa5ea32893b4f1e9d80d917d3d5eba2e4d10
Katie Vogel <kvog983@gmail.com>
2023-03-04 12:42:22 -0500

    cleaned up global variables, moved insert_all_stargazers to main(), removed unused code, comments

--- katies_apis.py
+++ katies_apis.py
@@ -6,40 +6,37 @@ import csv
 import secrets
 import psycopg2
 
-# create connection to db
-# conn = psycopg2.connect(database = 'katie_github', user = 'katievogel', host = '::1', port = '5432')
-db_config = {
-    'database' : 'katie_github', 
-    'user' : 'katievogel', 
-   ' host' : '::1', 
-    'port' : '5432'
-}
-
-# query to insert vals to db
-SQL = "INSERT INTO katie_github.stargazers (login, name) VALUES (%s, %s)"
-
-# set up github connection with auth token
-g = Github(secrets.GITHUB_TOKEN)
-
-# get user
-username = 'hyperfiddle'
-user = g.get_user(username)
-
-# get repos of user & check request data
-repo = g.get_repo('hyperfiddle/electric')
-
-# get name and login and write to db
-stargazers = repo.get_stargazers()
-
+# write to db
 def insert_stargazer(login, name):
     with psycopg2.connect(**db_config) as conn:
         with conn.cursor() as cur:
-            # star = stargazers[2]
-            cur.execute(SQL, (login, name))
+            cur.execute(
+                "INSERT INTO katie_github.stargazers (login, name) VALUES (%s, %s)", 
+                (login, name))
 
-def insert_all_stargazers():
-    for star in stargazers[:10]:
+def insert_all_stargazers(stargazers):
+    for star in stargazers[21:30]:
         insert_stargazer(star.login, star.name)
+
+
+db_config = {
+    'database' : 'katie_github', 
+    'user' : 'katievogel', 
+    'host' : '::1', 
+    'port' : '5432'
+}
+
+def main():
+    # set up github connection with auth token
+    g = Github(secrets.GITHUB_TOKEN)
+
+    # get repos of user & check request data
+    repo = g.get_repo('hyperfiddle/electric')
+    stargazers = repo.get_stargazers()
+    insert_all_stargazers(stargazers)
+
+if __name__ == "__main__": 
+    main()
 
 # get star user name, login from stargazers on repo for csv
 # stargazers = repo.get_stargazers()
