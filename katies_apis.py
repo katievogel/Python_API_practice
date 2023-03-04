3c3cddf04ef6d2ddaebe0d116d62aa7bf2e36751
Katie Vogel <kvog983@gmail.com>
2023-03-04 11:13:51 -0500

    setup psycopg2 connctions, inserts to db successfully from api

--- katies_apis.py
+++ katies_apis.py
@@ -7,7 +7,16 @@ import secrets
 import psycopg2
 
 # create connection to db
-conn = psycopg2.connect(database = 'postgres', user = 'katievogel', host = '::1', port = '5432')
+# conn = psycopg2.connect(database = 'katie_github', user = 'katievogel', host = '::1', port = '5432')
+db_config = {
+    'database' : 'katie_github', 
+    'user' : 'katievogel', 
+   ' host' : '::1', 
+    'port' : '5432'
+}
+
+# query to insert vals to db
+SQL = "INSERT INTO katie_github.stargazers (login, name) VALUES (%s, %s)"
 
 # set up github connection with auth token
 g = Github(secrets.GITHUB_TOKEN)
@@ -19,17 +28,30 @@ user = g.get_user(username)
 # get repos of user & check request data
 repo = g.get_repo('hyperfiddle/electric')
 
-# get star user name, login from stargazers on repo
+# get name and login and write to db
 stargazers = repo.get_stargazers()
-def get_stars_info():
-    records = []
-    for star in stargazers:
-        record = {
-            'name': star.name,
-            'login': star.login
-        } 
-        records.append(record)
-    return records
+
+def insert_stargazer(login, name):
+    with psycopg2.connect(**db_config) as conn:
+        with conn.cursor() as cur:
+            # star = stargazers[2]
+            cur.execute(SQL, (login, name))
+
+def insert_all_stargazers():
+    for star in stargazers[:10]:
+        insert_stargazer(star.login, star.name)
+
+# get star user name, login from stargazers on repo for csv
+# stargazers = repo.get_stargazers()
+# def get_stars_info():
+#     records = []
+#     for star in stargazers:
+#         record = {
+#             'name': star.name,
+#             'login': star.login
+#         } 
+#         records.append(record)
+#     return records
 
 
 # write star counts to csv
@@ -37,12 +59,13 @@ def get_stars_info():
 # writer.writeheader
 # create records dict to write to csv
 # writer.writerows
-with open('./repo_stars.csv', 'w') as file:
-    fieldnames = ['name', 'login']
-    writer = csv.DictWriter(file, fieldnames)
-    writer.writeheader()
-    records = get_stars_info()
-    writer.writerows(records)
+# with open('./repo_stars.csv', 'w') as file:
+#     fieldnames = ['name', 'login']
+#     writer = csv.DictWriter(file, fieldnames)
+#     writer.writeheader()
+#     records = get_stars_info()
+#     writer.writerows(records)
+
 
 
 
